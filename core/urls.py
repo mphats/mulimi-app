9719518c0436022277400ada4df3b06fe4266b37
mphats <mphatsos45@gmail.com>
2025-11-28 22:30:37 +0200

    Add namespace to users URLs to fix NoReverseMatch error

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -7,7 +9,11 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/v1/", include("api.urls")),
-    path("api/v1/auth/", include("users.urls")),
+    path("api/v1/auth/", include(("users.urls", "users"), namespace="users")),
     # register frontend with namespace so `{% url 'frontend:...'%}` resolves
     path("", include(("frontend.urls", "frontend"), namespace="frontend")),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
